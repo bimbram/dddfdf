d456caed00fea7ae119615ea3d331172d3eb48d6
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-09-19 03:47:05 +0700

    022 - FizzBuzz Problem Statement

--- 07 - The Classic FizzBuzz/fizzbuzz/index.js
+++ 07 - The Classic FizzBuzz/fizzbuzz/index.js
@@ -12,6 +12,19 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// My solution
+function fizzBuzz(n) {
+  for(var x = 1; x <= n; x ++) {
+    if((x % 3 === 0) && (x % 5 === 0)) {
+      console.log("fizzbuzz");
+    } else if(x % 5 === 0) {
+      console.log("buzz");
+    } else if(x % 3 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(x);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
