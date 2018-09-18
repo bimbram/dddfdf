7c86af1d0463cb8b7d20980ef35e875bdd3d0d18
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-09-19 01:06:44 +0700

    017 - Reversing an Int Solution

--- 05 - Integer Reversal/reverseint/index.js
+++ 05 - Integer Reversal/reverseint/index.js
@@ -7,7 +7,7 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
-
+/*
 // My solution
 function reverseInt(n) {
   if(n < 0 ) {
@@ -19,7 +19,11 @@ function reverseInt(n) {
     return Number(reverseInt);
   }
 }
-
-
+*/
+// Grider's solution
+function reverseInt(n) {
+  const reversed = n.toString().split('').reverse().join('');
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
