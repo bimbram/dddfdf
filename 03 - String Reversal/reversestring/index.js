0a8ee99dd7da8a428c82a00c0f0a9eee12434a08
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-09-18 15:28:31 +0700

    009 - String Reversal Solution 1

--- 03 - String Reversal/reversestring/index.js
+++ 03 - String Reversal/reversestring/index.js
@@ -6,9 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+/*
+// My solution (first try)
 function reverse(str) {
-  return str.split();
+  return str.split("").reverse().join("");
+}
 
+// First solution
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+
+*/
+// My solution using a loop
+function reverse(str) {
+  var newStr = ""
+  var counter = 1
+  while(newStr.length < str.length) {
+    newStr += str[str.length-counter]
+	counter++
+  }
+  return newStr;
 }
 
 module.exports = reverse;
