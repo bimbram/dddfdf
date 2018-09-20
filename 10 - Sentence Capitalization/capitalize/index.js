e43c248d311b4014fdf1c27c3739f91f789a0f79
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-09-20 15:13:20 +0700

    033 - Capitalization Solutioni 1

--- 10 - Sentence Capitalization/capitalize/index.js
+++ 10 - Sentence Capitalization/capitalize/index.js
@@ -14,4 +14,14 @@ function capitalize(str) {
   }).join(' ');
 }
 
+// First solution
+function capitalize(str) {
+  const words = [];
+
+  for(let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(' ');
+}
+
 module.exports = capitalize;
