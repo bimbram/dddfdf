da0d561e3227f7a8ae453bc21ee244ab9aa9449e
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-09-27 13:11:20 +0700

    035 - The  Steps Question

--- 11_Printing_Steps/steps/index.js
+++ 11_Printing_Steps/steps/index.js
@@ -17,6 +17,25 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// My solution
+function steps(n) {
+  //var chars = ''
+  for(var i = 0; i < n; i ++) {
+    //chars += '#';
+    //console.log(chars + spaces(n - i - 1));
+    console.log('#'.repeat(i+1) + " ".repeat(n-i-1));
+  }
+}
+
+function spaces(n) {
+  var stringSpaces = ''
+  for(var i = 0; i < n; i++) {
+    stringSpaces += ' ';
+  }
+  return stringSpaces;
+}
+
+
+
 
 module.exports = steps;
