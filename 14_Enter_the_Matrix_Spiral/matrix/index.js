2fc8874251132da8f2d70516135522f819f9fba6
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-11-05 01:50:45 +0700

    048 - More on Spiral

--- 14_Enter_the_Matrix_Spiral/matrix/index.js
+++ 14_Enter_the_Matrix_Spiral/matrix/index.js
@@ -26,14 +26,48 @@
 
 // Grider's iterative solution
 function matrix(n) {
+    const results = [];
+    for(let i = 0; i < n; i++) {
+	results.push([]);
+    }
+
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+	// Top row
+	for (let i = startColumn; i <= endColumn; i++) {
+	    results[startRow][i] = counter;
+	    counter++;
+	}
+	startRow++;
+	
+	// Right column
+	for (let i = startRow; i <= endRow; i++) {
+	    results[i][endColumn] = counter;
+	    counter++;
+	}
+	endColumn--;
+
+	// Bottom row
+	for (let i = endCollumn; i >= startColumn; i--) {
+	    results[endRow][i] = counter;
+	    counter++;
+	}
+	endRow--;
+	
+	// start column
+	for (let i = endRow; i >= startRow; i--) {
+	    results[i][startCollumn] = counter;
+	    counter++;
+	}
+	startCollumn++;
+    }
     
 }
-
-
-// Grider's recursive solution
-
-
-
 
 // My iterative solution
 function matrix(n) {
@@ -243,6 +277,7 @@ function isNextIndexEmptyAndWithinRange(container, currentRow, currentCollumn, d
     }
     return isIndexWithinRange && isNextIndexEmpty ? true : false;
 }
+
 
 
 
